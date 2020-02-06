6bbbe516866cb2710bb7760acb97eea9791f7fd4
sheng855174 <sheng855174@gmail.com>
2020-02-06 09:57:29 +0800

    SDK 0.1.0 1.新增第三方的Onclick事件

--- app/src/main/java/tw/playground/sheng/MainActivity.java
+++ app/src/main/java/tw/playground/sheng/MainActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import tw.playground.sheng.http.post.HttpsPostThread;
 import tw.playground.sheng.http.post.ILoginHttpsPostAdapter;
 
 public class MainActivity extends AppCompatActivity {
     private Button login_button;
+    private ImageView imageView_twitter;
+    private ImageView imageView_instagram;
+    private ImageView imageView_facebook;
     private EditText username;
     private EditText password;
 
@@ -28,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
         login_button = findViewById(R.id.LoginButton);
         username = findViewById(R.id.Username);
         password = findViewById(R.id.Password);
+        imageView_twitter = findViewById(R.id.imageView_twitter);
+        imageView_instagram = findViewById(R.id.imageView_instagram);
+        imageView_facebook = findViewById(R.id.imageView_facebook);
 
         //設定監聽器
         login_button.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +44,24 @@ public class MainActivity extends AppCompatActivity {
                 ILoginHttpsPostAdapter iLoginHttpsPostAdapter = new HttpsPostThread();
                 iLoginHttpsPostAdapter.login(username.getText().toString(), password.getText().toString());
                 finish();
+            }
+        });
+        imageView_twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"this is twitter",Toast.LENGTH_LONG).show();
+            }
+        });
+        imageView_instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"this is instagram",Toast.LENGTH_LONG).show();
+            }
+        });
+        imageView_facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"this is facebook",Toast.LENGTH_LONG).show();
             }
         });
 
